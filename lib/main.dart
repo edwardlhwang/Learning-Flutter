2bc8f7d6edaff147ad5b92ac1d26438d239e2132
edwardlhwang <edwardhwang@berkeley.edu>
2020-05-21 21:17:55 -0700

    testing feature branch

--- lib/main.dart
+++ lib/main.dart
@@ -60,6 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
               )
             ),
             Text(
